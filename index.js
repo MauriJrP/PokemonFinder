82eb06c5d40f718491373b4358b8291a1bb47e8c
Mauricio Rodriguez <maurijrp2001@gmail.com>
2020-07-29 19:31:25 -0500

    Background color added to pokemon list, also div features of pokemon created in pokemon list and autocomplete of imput delete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 buscar = document.getElementById("buscar");
+pokemon__right = document.getElementsByClassName("pokemon__right")[0]
+
 buscar.addEventListener("click", () => {
-    console.log(`click`);
+    // console.log(`click`);
     pokemonElegido = document.getElementById("pokemon__elegido");
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemonElegido.value}/`;
     const pokemon = document.getElementById("pokemon__name");
@@ -14,9 +16,11 @@ buscar.addEventListener("click", () => {
             pokemon.innerHTML = `pokemon: ${data.name}`;
             imagen.src = data.sprites.front_default;
             console.log(data);
+            pokemon__right.className = "pokemon__right"
         })
         .catch((error) => {
             pokemon.innerHTML = "Pokemon no encontrado";
             imagen.src = "";
+            pokemon__right.className = "pokemon__right none"
         });
 });
