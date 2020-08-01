c7b194dd4890934b16ec84f51afd0889e4c29bb6
Mauricio Rodriguez <maurijrp@gmail.com>
2020-07-31 22:09:11 -0500

    Refactor to getPokemon function

--- index.js
+++ index.js
@@ -1,26 +1,54 @@
-buscar = document.getElementById("buscar");
-pokemon__right = document.getElementsByClassName("pokemon__right")[0]
+const buscar = document.getElementById("buscar");
+const pokemon__right = document.getElementsByClassName("pokemon__right")[0];
 
-buscar.addEventListener("click", () => {
-    console.log(`click`);
+buscar.addEventListener("click", getPokemon);
+
+async function getPokemon() {
     pokemonElegido = document.getElementById("pokemon__elegido");
     const url = `https://pokeapi.co/api/v2/pokemon/${pokemonElegido.value}/`;
     const pokemon = document.getElementById("pokemon__name");
     const imagen = document.getElementById("pokemon__image");
 
-    fetch(url)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            pokemon.innerHTML = `pokemon: ${data.name}`;
-            imagen.src = data.sprites.front_default;
-            console.log(data);
-            pokemon__right.className = "pokemon__right"
-        })
-        .catch((error) => {
-            pokemon.innerHTML = "Pokemon no encontrado";
-            imagen.src = "";
-            pokemon__right.className = "pokemon__right none"
-        });
-});
+    try {
+        // debugger
+        const request = await fetch(url);
+        const requestJSON = await request.json();
+        drawPokemon(requestJSON, pokemon, imagen);
+        console.log(requestJSON);
+    } catch (error) {
+        clearScreen(error, pokemon, imagen)
+
+    }
+}
+
+const drawPokemon =  ( requestJSON, pokemon, imagen ) => {
+    pokemon.innerHTML = `pokemon: ${requestJSON.name}`;
+    imagen.src = requestJSON.sprites.front_default;
+    pokemon__right.className = "pokemon__right";
+}
+
+const clearScreen =  ( error, pokemon, imagen ) => {
+    pokemon.innerHTML = "Pokemon no encontrado";
+    console.error( error )
+    imagen.src = "";
+    pokemon__right.className = "pokemon__right none";
+}
+
+
+// async function request(url, pokemon, imagen) {
+//     fetch(url)
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then((data) => {
+//         pokemon.innerHTML = `pokemon: ${data.name}`;
+//         imagen.src = data.sprites.front_default;
+//         console.log(data);
+//         pokemon__right.className = "pokemon__right"
+//     })
+//     .catch((error) => {
+//         pokemon.innerHTML = "Pokemon no encontrado";
+//         imagen.src = "";
+//         pokemon__right.className = "pokemon__right none"
+//     });
+// }
