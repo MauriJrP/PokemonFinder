f799ed12d9e7b55cf954f6ad9a8f424d75e86f7f
Mauricio Rodriguez <maurijrp@gmail.com>
2020-08-09 20:12:16 -0500

    Pokemon height and weight added, features section styles improved

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const buscar = document.getElementById("buscar");
-const pokemon__right = document.getElementsByClassName("pokemon__right")[0];
+const buscar = document.getElementById('buscar');
+const pokemon__right = document.getElementsByClassName('pokemon__right')[0];
 const pokemon__features = document.getElementsByClassName(
-  "pokemon__features"
+  'pokemon__features'
 )[0].children;
-console.log(pokemon__features);
+// console.log(pokemon__features);
 
-buscar.addEventListener("click", getPokemon);
+buscar.addEventListener('click', getPokemon);
 
 async function getPokemon() {
-  pokemonElegido = document.getElementById("pokemon__elegido");
+  pokemonElegido = document.getElementById('pokemon__elegido');
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemonElegido.value}/`;
-  const pokemon = document.getElementById("pokemon__name");
-  const imagen = document.getElementById("pokemon__image");
+  const pokemon = document.getElementById('pokemon__name');
+  const imagen = document.getElementById('pokemon__image');
 
   try {
     // debugger
@@ -26,17 +26,23 @@ async function getPokemon() {
 const drawPokemon = (requestJSON, pokemon, imagen) => {
   pokemon.innerHTML = `pokemon: ${requestJSON.name}`;
   imagen.src = requestJSON.sprites.front_default;
-  pokemon__right.className = "pokemon__right";
-  pokemon__features[0].innerHTML = `<strong>Tipo:<br></strong>${requestJSON.types[0].type.name}`;
-  pokemon__features[1].innerHTML = `<strong>Habilidades:</strong><br>1.-${requestJSON.abilities[0].ability.name}<br>2.-${requestJSON.abilities[1].ability.name}`;
-  console.log(requestJSON.abilities[0].ability.name);
+  pokemon__right.className = 'pokemon__right';
+  pokemon__features[0].innerHTML = `<strong>Tipo</strong><br>&nbsp${requestJSON.types[0].type.name}`;
+  pokemon__features[1].innerHTML = `<strong>Habilidades</strong><br>&nbsp1.-${requestJSON.abilities[0].ability.name}<br>&nbsp2.-${requestJSON.abilities[1].ability.name}`;
+  pokemon__features[2].innerHTML = `<strong>Height</strong><br>&nbsp${
+    requestJSON.height * 6
+  }cm.`;
+  pokemon__features[3].innerHTML = `<strong>Weight</strong><br>&nbsp${
+    requestJSON.weight / 10
+  }kg.`;
+  console.log(requestJSON);
 };
 
 const clearScreen = (error, pokemon, imagen) => {
-  pokemon.innerHTML = "Pokemon no encontrado";
+  pokemon.innerHTML = 'Pokemon no encontrado';
   console.error(error);
-  imagen.src = "";
-  pokemon__right.className = "pokemon__right none";
+  imagen.src = '';
+  pokemon__right.className = 'pokemon__right none';
 };
 
 // async function request(url, pokemon, imagen) {
