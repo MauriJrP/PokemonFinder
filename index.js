adda24b499de73d92876a32c2bc001a343edfc3d
Mauricio Rodriguez (w) <maurijrp2001@gmail.com>
2021-01-16 23:59:30 -0600

    main section improved and pokemon list section started

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const pokemon__right = document.getElementsByClassName('pokemon__right')[0];
 const pokemon__features = document.getElementsByClassName(
   'pokemon__features'
 )[0].children;
-// console.log(pokemon__features);
 
 buscar.addEventListener('click', getPokemon);
 
@@ -11,21 +10,26 @@ async function getPokemon() {
   pokemonElegido = document.getElementById('pokemon__elegido');
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemonElegido.value.toLowerCase()}/`;
   const pokemon = document.getElementById('pokemon__name');
-  const imagen = document.getElementById('pokemon__image');
+  const imagen = document.getElementsByClassName('pokemon__left')[0];
+  const html = document.implementation.createHTMLDocument();
 
   try {
-    // debugger
     const request = await fetch(url);
     const requestJSON = await request.json();
-    drawPokemon(requestJSON, pokemon, imagen);
+    drawMainPokemon(requestJSON, pokemon, imagen, html);
   } catch (error) {
-    clearScreen(error, pokemon, imagen);
+    clearScreen(error, pokemon, imagen, html);
   }
 }
 
-const drawPokemon = (requestJSON, pokemon, imagen) => {
+const drawMainPokemon = (requestJSON, pokemon, imagen, html) => {
   pokemon.innerHTML = `pokemon: ${requestJSON.name}`;
-  imagen.src = requestJSON.sprites.front_default;
+  html.body.innerHTML = `<img id="pokemon__image" class="pokemon__image" src="${requestJSON.sprites.front_default}" alt="pokemon" />`;
+  if (imagen.children[1]) {
+    imagen.children[1].remove();
+  }
+  imagen.append(html.body.children[0]);
+
   pokemon__right.className = 'pokemon__right';
   pokemon__features[0].innerHTML = `<strong>Tipo</strong><br>&nbsp${requestJSON.types[0].type.name}`;
   pokemon__features[1].innerHTML = `<strong>Habilidades</strong><br>&nbsp1.-${requestJSON.abilities[0].ability.name}<br>&nbsp2.-${requestJSON.abilities[1].ability.name}`;
@@ -38,27 +42,41 @@ const drawPokemon = (requestJSON, pokemon, imagen) => {
   console.log(requestJSON);
 };
 
-const clearScreen = (error, pokemon, imagen) => {
+const clearScreen = (error, pokemon, imagen, html) => {
   pokemon.innerHTML = 'Pokemon no encontrado';
   console.error(error);
-  imagen.src = '';
+  if (imagen.children[1]) {
+    imagen.children[1].remove();
+  }
   pokemon__right.className = 'pokemon__right none';
 };
 
-// async function request(url, pokemon, imagen) {
-//     fetch(url)
-//     .then((response) => {
-//         return response.json();
-//     })
-//     .then((data) => {
-//         pokemon.innerHTML = `pokemon: ${data.name}`;
-//         imagen.src = data.sprites.front_default;
-//         console.log(data);
-//         pokemon__right.className = "pokemon__right"
-//     })
-//     .catch((error) => {
-//         pokemon.innerHTML = "Pokemon no encontrado";
-//         imagen.src = "";
-//         pokemon__right.className = "pokemon__right none"
-//     });
+// -------- ------- ------ ----- revisar ----- ------ ------- --------
+
+// // -------- ------- ------ ----- Pokemon List ----- ------ ------- --------
+// const pokemonPage =  async (  ) => {
+//   // let pokemones = 1;
+//   let counter = 0;
+//   let url =  `https://pokeapi.co/api/v2/pokemon/?offset=${counter}&limit=20`
+//   console.log( `jdfsofij` )
+
+//   return {
+//     getPage: async  (  ) => {
+//       try {
+//         request = await fetch(url);
+//         requestJSON = await request.json();
+//         console.log( `${request}` )
+//         counter += 20;
+//         return requestJSON;
+//       } catch (error) {
+//         console.log( `${error}` )
+//       }
+//     }
+//   }
 // }
+
+// const pagination = pokemonPage();
+// // console.log( `${pagination.getPage}` )
+
+// const loadMore = document.getElementsByClassName('section-list__button')[0];
+// // loadMore.addEventListener = 
