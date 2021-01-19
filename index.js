0d42dcffabcaa5d4cb82636a19db2b808a7b56cf
Mauricio Rodriguez (w) <maurijrp2001@gmail.com>
2021-01-18 20:11:45 -0600

    ads

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ const pokemon__features = document.getElementsByClassName(
 )[0].children;
 
 buscar.addEventListener('click', getPokemon);
+// console.log(pokemonElegido)
+// console.log('asfd')
 
-async function getPokemon() {
-  pokemonElegido = document.getElementById('pokemon__elegido');
-  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonElegido.value.toLowerCase()}/`;
+async function getPokemon(poke, prueba = null) {
+  // if (!prueba) {
+  //   const poke = document.getElementById('pokemon__elegido').value.toLowerCase();
+  //   console.log(poke);
+  // } else {
+  //   const url = `https://pokeapi.co/api/v2/pokemon/${poke}/`;
+  // }
+  const url = prueba ? `https://pokeapi.co/api/v2/pokemon/${poke}/` : `https://pokeapi.co/api/v2/pokemon/${document.getElementById('pokemon__elegido').value.toLowerCase()}/`;
   const pokemon = document.getElementById('pokemon__name');
   const imagen = document.getElementsByClassName('pokemon__left')[0];
   const html = document.implementation.createHTMLDocument();
@@ -16,7 +23,6 @@ async function getPokemon() {
   try {
     const request = await fetch(url);
     const requestJSON = await request.json();
-    // console.log( requestJSON )
     drawMainPokemon(requestJSON, pokemon, imagen, html);
   } catch (error) {
     clearScreen(error, pokemon, imagen, html);
@@ -33,6 +39,12 @@ const drawMainPokemon = (requestJSON, pokemon, imagen, html) => {
 
   pokemon__right.className = 'pokemon__right';
   pokemon__features[0].innerHTML = `<strong>Tipo</strong><br>&nbsp${requestJSON.types[0].type.name}`;
+  // let abilitiesString = ``;
+  // requestJSON.abilities.forEach( ( abilitie ) => {
+  //   abilitiesString += `<strong>Habilidades</strong><br>&nbsp1.-${requestJSON.abilities[0].ability.name}<br>&nbsp2.-${requestJSON.abilities[1].ability.name}`;
+    
+  // })
+  // pokemon__features[1].innerHTML = abilitiesString;
   pokemon__features[1].innerHTML = `<strong>Habilidades</strong><br>&nbsp1.-${requestJSON.abilities[0].ability.name}<br>&nbsp2.-${requestJSON.abilities[1].ability.name}`;
   pokemon__features[2].innerHTML = `<strong>Height</strong><br>&nbsp${
     requestJSON.height * 6
@@ -52,12 +64,11 @@ const clearScreen = (error, pokemon, imagen, html) => {
   pokemon__right.className = 'pokemon__right none';
 };
 
-// -------- ------- ------ ----- revisar ----- ------ ------- --------
-
 // -------- ------- ------ ----- Pokemon List ----- ------ ------- --------
+
 const pokemonPage =  async (  ) => {
   let counter = 1;
-  // let url =  `https://pokeapi.co/api/v2/pokemon/?offset=${counter}&limit=20`     // page
+  // let url =  `https://pokeapi.co/api/v2/pokemon/?offset=${counter}&limit=20`
   let urlBase =  `https://pokeapi.co/api/v2/pokemon/`;
 
   return {
@@ -84,18 +95,16 @@ const pokemonPage =  async (  ) => {
     pokemonTemplate:  ( page ) => {
       let template = [];
       for ( let i = 0; i <= 19; i++ ) {
-        template[i] = `<div class="section-list__div-item" id="${page.names[i]}">
-                        <p class="section-list__p">${page.names[i]}</p>
-                        <img src="${page.images[i]}" class="section-list__img" alt="${page.names[i]}">
+        const name = page.names[i]
+        template[i] = `<div class="${name} section-list__div-item">
+                        <p class="${name} section-list__p">${name}</p>
+                        <img src="${page.images[i]}" class="${name} section-list__img" alt="${name}">
                       </div>`
       }
       return template;
     }
   }
 }
-
-// const page2 = await pokemonPageGenerator.getPage();
-// console.log( page2.names );
 
 const pokemonList = async () => {
   const pokemonPageGenerator =  await pokemonPage();
@@ -111,11 +120,12 @@ const pokemonList = async () => {
         html.body.innerHTML = pokemon
         sectionList.append(html.body.children[0]);
         sectionList.lastChild.addEventListener('click', ( pokemon ) => {
-          console.log( pokemon )
+          const poke = pokemon.target.className.split(' ')[0];
+          getPokemon(poke, 1);
+          // console.log('adsf')
+          console.log( poke )
         })
-        // console.log( sectionList.lastChild )
       })
-      // console.log(sectionList)
     }
   }
 }
@@ -127,12 +137,7 @@ async function adf () {
   const loadMore = document.getElementsByClassName('section-list__button')[0];
   loadMore.addEventListener('click',  async (  ) => {
     await asd.drawPage()
-    // console.log( `asdf` )
   })
-  // await asd.drawPage();
 
 }
 adf();
-
-// const 6
-// loadMore.addEventListener = d
